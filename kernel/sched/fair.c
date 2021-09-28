d3b36caf197fa1c8d3611a982c74f0a0eccb00bc
Stoner <jaycopter@foxmail.com>
2021-09-28 21:39:05 +0800

    sched/fair: Fix cpu_util_without() compilation with WALT

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6677,7 +6677,9 @@ done:
  */
 static int cpu_util_without(int cpu, struct task_struct *p)
 {
+#ifndef CONFIG_SCHED_WALT
 	struct cfs_rq *cfs_rq;
+#endif
 	unsigned int util;
 
 #ifdef CONFIG_SCHED_WALT
